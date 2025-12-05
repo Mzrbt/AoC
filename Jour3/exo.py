1967660ff7e55a79c419d2fa521ea72363b6e076
Mzrbt <jabetmarin07@gmail.com>
2025-12-05 15:13:55 +0100

    Jour3 etape1

---
+++ Jour3/exo.py
@@ -0,0 +1,17 @@
+with open("Jour3/input.txt") as f:
+    input = f.readlines()
+
+res=0
+for elt in input:
+    imax1 = 0
+    max1 = 0
+    max2 = 0
+    for i in range(len(elt)-1):
+        if int(elt[i]) > max1 and i < (len(elt)-2):
+            max1 = int(elt[i])
+            imax1 = i
+    for j in range(len(elt)-1):
+        if int(elt[j]) > max2 and j != imax1 and j > imax1:
+            max2 = int(elt[j])
+    res += int(''.join([str(max1),str(max2)]))
+print(res)
